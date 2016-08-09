9ed06ba578b35df7a1006df3c62c0e9a912e9053
Tim Gion <timothy.gion@jhuapl.edu>
2016-08-09 14:55:02 -0400

    Unset PYTHONPATH before running Python3.4. AWS places a folder in PYTHONPATH that breaks Python 3.4's import of concurrent.futures.

--- lambda/lambda_loader.py
+++ lambda/lambda_loader.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import os
 import subprocess
 LAMBDA_PATH_PREFIX = "local/lib/python3.4/site-packages/lambda/"
 lambda_dictionary = { "s3_flush": LAMBDA_PATH_PREFIX + "s3_flush_lambda.py",
@@ -19,7 +20,14 @@ def handler(event, context):
     json_event = json.dumps(event)
     print("event: " + json_event)
     args = ("bin/python3.4", lambda_path, json_event)
-    popen = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # AWS defines a PYTHONPATH that breaks our Python 3.4 code.  We can't
+    # import concurrent.futures, specifically.
+    env = os.environ
+    del env['PYTHONPATH']
+    popen = subprocess.Popen(
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        env=env)
     popen.wait()
     output = popen.stdout.read()
     err_str = popen.stderr.read()
