7f744839f32ae65823b875dcb349f807be4b565e
Sandy Hider <sandy.hider@jhuapl.edu>
2016-09-07 15:41:02 -0400

    updated sqs_watcher to invoke multiple lambdas if the queue is out of balance up to a maximum of MAX_LAMBDAS_TO_INVOKE which is currently set to 10

--- cachemgr/boss_sqs_watcherd.py
+++ cachemgr/boss_sqs_watcherd.py
@@ -31,6 +31,7 @@ import json
 
 from bossutils import configuration
 from bossutils import daemon_base
+from bossutils import logger
 from bossutils.aws import get_region
 
 """
@@ -38,10 +39,13 @@ Daemon to monitor the S3_Flush SQS Queue.  If the ApproximateNumberOfMessages is
 daemon will invoke new lambdas to return the ApproximateNumberOfMessages back to zero.
 """
 
+MAX_LAMBDAS_TO_INVOKE = 10
+
 class SqsWatcherDaemon(daemon_base.DaemonBase):
 
     def __init__(self, pid_file_name, pid_dir="/var/run"):
         super().__init__(pid_file_name, pid_dir)
+        self.log = logger.BossLogger().logger
         self.config = configuration.BossConfig()
 
         # kvio settings
@@ -68,10 +72,11 @@ class SqsWatcherDaemon(daemon_base.DaemonBase):
                             "lambda-name": "s3_flush"}
 
     def run(self):
-        self.initialize()
+        old_message_num = 0
+        message_num = 0
+
 
         while True:
-            time.sleep(15)
             client = boto3.client('sqs', region_name=get_region())
             response = client.get_queue_attributes(
                 QueueUrl=self.config["aws"]["s3-flush-queue"],
@@ -79,20 +84,29 @@ class SqsWatcherDaemon(daemon_base.DaemonBase):
                     'ApproximateNumberOfMessages', 'ApproximateNumberOfMessagesNotVisible'
                 ]
             )
-            old_message_num = message_num;
-            message_num = int(response['ApproximateNumberOfMessages'])
-            self.log.info("boss-sqs-watcherd: checking sqs queue current messages: {}  prvious messages: {}".format(
+            if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+                self.log.error("sqs_watcherd get_queue_attributes failed. Response HTTPSStatusCode: " + str(
+                    response['ResponseMetadata']['HTTPStatusCode']))
+                time.sleep(15)
+                continue
+
+            old_message_num = message_num
+            message_num = int(response['Attributes']['ApproximateNumberOfMessages'])
+            self.log.debug("boss-sqs-watcherd: checking sqs queue current messages: {}  previous messages: {}".format(
                 message_num, old_message_num))
 
-            #if ((message_num != 0) and (message_num == old_message_num)):
-            client = boto3.client('lambda', region_name=get_region())
-            self.log.info("kicking off lambda")
-            response = client.invoke(
-                FunctionName=self.config["lambda"]["flush_function"],
-                InvocationType='Event',
-                Payload=json.dumps(self.lambda_data).encode())
-
-            self.log.info("sqs_watcherd lambda response: " + str(response))
+            if ((message_num != 0) and (message_num == old_message_num)):
+                client = boto3.client('lambda', region_name=get_region())
+                self.log.info("kicking off lambda")
+                lambdas_to_invoke = min(message_num, MAX_LAMBDAS_TO_INVOKE)
+                for i in range(lambdas_to_invoke):
+                    response = client.invoke(
+                        FunctionName=self.config["lambda"]["flush_function"],
+                        InvocationType='Event',
+                        Payload=json.dumps(self.lambda_data).encode())
+                    if response['ResponseMetadata']['HTTPStatusCode'] != 202:
+                        self.log.error("sqs_watcherd invoke_lambda failed. Response HTTPSStatusCode: " + str(response['ResponseMetadata']['HTTPStatusCode']))
+            time.sleep(15)
 
 
 if __name__ == '__main__':
