fd72b8170847f608e09673386039fe5973835ce0
Derek Pryor <Derek.Pryor@jhuapl.edu>
2016-09-29 09:48:41 -0400

    Broken KeyCloakClient unittests into multiple test classes. Implemented unittests for login / logout.

--- bossutils/test/test_keycloak.py
+++ bossutils/test/test_keycloak.py
@@ -31,13 +31,17 @@ import hvac
 
 REALM = 'REALM'
 BASE = 'http://auth'
-TOKEN = {'access_token': 'xxxxxx', 'refresh_token': 'xxxxxx'}
+TOKEN = {'access_token': 'access_token', 'refresh_token': 'refresh_token'}
 
 def mock_login(self):
     self.token = TOKEN
+    self.client_id = 'client_id'
+    self.login_realm = 'realm'
 
 def mock_logout(self):
     self.token = None
+    self.client_id = None
+    self.login_realm = None
 
 class MockResponse:
     def __init__(self, status, text):
@@ -98,12 +102,161 @@ class TestKeyCloakError(unittest.TestCase):
     pass
 '''
 
-# DP TODO: create three different test classes
-#          tests for login / logout (authentication) (stubb out vault / requests)
-#          test get/post/put/delete (stubb out requests / KC login & logout)
-#          test KC methods (stubb out get/post/put/delete KC methods)
+class TestAuthentication(unittest.TestCase):
+    def setUp(self):
+        self.client = KeyCloakClient(REALM, BASE)
 
-class TestKeyCloakClient(unittest.TestCase):
+    @mock.patch(prefix + 'keycloak.Vault', autospec = True)
+    @mock.patch(prefix + 'keycloak.requests.post', autospec = True)
+    def test_login(self, mPost, mVault):
+        """login makes post call with data from vault and creates token variable"""
+        mPost.return_value = MockResponse(200, json.dumps(TOKEN))
+        mVault.return_value.read = lambda p, k: k
+
+        self.assertIsNone(self.client.token)
+
+        self.client.login()
+
+        self.assertEqual(self.client.token, TOKEN)
+
+        # DP NOTE: since the post call contains information read from Vault
+        #          I don't explicitly check for the calls to Vault, because
+        #          if they didn't happen, they the post call assert will fail
+
+        url = BASE + '/realms/realm/protocol/openid-connect/token'
+        data = {'grant_type': 'password',
+                'client_id': 'client_id',
+                'username': 'username',
+                'password': 'password' }
+        call = mock.call(url, data = data, verify = True)
+        self.assertEqual(mPost.mock_calls, [call])
+
+    @mock.patch(prefix + 'keycloak.Vault', autospec = True)
+    @mock.patch(prefix + 'keycloak.requests.post', autospec = True)
+    def test_failed_login(self, mPost, mVault):
+        """on post response with error status, KeyCloakError is raised"""
+        mPost.return_value = MockResponse(500, '[]')
+        mVault.return_value.read = lambda p, k: k
+
+        with self.assertRaises(KeyCloakError):
+            self.client.login()
+
+    @mock.patch(prefix + 'keycloak.requests.post', autospec = True)
+    def test_logout(self, mPost):
+        """logout makes post call with data used in login and clears variables"""
+        mPost.return_value = MockResponse(200, '[]')
+
+        self.client.login = MethodType(mock_login, self.client) # See comment in TestRequests.setUp()
+        self.client.login()
+
+        self.assertEqual(self.client.token, TOKEN)
+
+        self.client.logout()
+
+        self.assertIsNone(self.client.token)
+
+        url = BASE + '/realms/realm/protocol/openid-connect/logout'
+        data = {'refresh_token': 'refresh_token',
+                'client_id': 'client_id' }
+        call = mock.call(url, data = data, verify = True)
+        self.assertEqual(mPost.mock_calls, [call])
+
+    @mock.patch(prefix + 'keycloak.requests.post', autospec = True)
+    def test_failed_logout(self, mPost):
+        """on post response with error status, KeyCloakError is raised"""
+        mPost.return_value = MockResponse(500, '[]')
+
+        self.client.login = MethodType(mock_login, self.client) # See comment in TestRequests.setUp()
+        self.client.login()
+
+        with self.assertRaises(KeyCloakError):
+            self.client.logout()
+
+    def test_with(self):
+        """login and logout are called"""
+        self.client.loginCalled = False
+        self.client.logoutCalled = False
+
+        def call_login(self):
+            self.loginCalled = True
+
+        def call_logout(self):
+            self.logoutCalled = True
+
+        self.client.login = MethodType(call_login, self.client)
+        self.client.logout = MethodType(call_logout, self.client)
+
+        with self.client as kc:
+            pass
+
+        self.assertTrue(self.client.loginCalled)
+        self.assertTrue(self.client.logoutCalled)
+
+    def test_failed_with(self):
+        """if an exception during with then it is propogated correctly"""
+        self.client.loginCalled = False
+        self.client.logoutCalled = False
+
+        def call_login(self):
+            self.loginCalled = True
+
+        def call_logout(self):
+            self.logoutCalled = True
+
+        self.client.login = MethodType(call_login, self.client)
+        self.client.logout = MethodType(call_logout, self.client)
+
+        with self.assertRaises(Exception):
+            with self.client as kc:
+                raise Exception()
+
+        self.assertTrue(self.client.loginCalled)
+        self.assertTrue(self.client.logoutCalled)
+
+    def test_failed_with_login(self):
+        """if login fails, logout is not called"""
+        self.client.loginCalled = False
+        self.client.logoutCalled = False
+
+        def call_login(self):
+            self.loginCalled = True
+            raise Exception()
+
+        def call_logout(self):
+            self.logoutCalled = True
+
+        self.client.login = MethodType(call_login, self.client)
+        self.client.logout = MethodType(call_logout, self.client)
+
+        with self.assertRaises(Exception):
+            with self.client as kc:
+                pass
+
+        self.assertTrue(self.client.loginCalled)
+        self.assertFalse(self.client.logoutCalled)
+
+    def test_failed_with_logout(self):
+        """if logout fails, no exception is generated"""
+        self.client.loginCalled = False
+        self.client.logoutCalled = False
+
+        def call_login(self):
+            self.loginCalled = True
+
+        def call_logout(self):
+            self.logoutCalled = True
+            raise Exception()
+
+        self.client.login = MethodType(call_login, self.client)
+        self.client.logout = MethodType(call_logout, self.client)
+
+        with self.client as kc:
+            pass
+
+        self.assertTrue(self.client.loginCalled)
+        self.assertTrue(self.client.logoutCalled)
+
+class TestRequests(unittest.TestCase):
     def setUp(self):
         self.client = KeyCloakClient(REALM, BASE)
 
@@ -113,38 +266,6 @@ class TestKeyCloakClient(unittest.TestCase):
         # See http://stackoverflow.com/questions/972/adding-a-method-to-an-existing-object-instance
         self.client.login = MethodType(mock_login, self.client)
         self.client.logout = MethodType(mock_logout, self.client)
-
-    def test_login(self):
-        """login makes post call with data from vault and creates token variable"""
-        pass
-
-    def test_failed_login(self):
-        """on post response with error status, KeyCloakError is raised"""
-        pass
-
-    def test_logout(self):
-        """logout makes post call with data used in login and clears variables"""
-        pass
-
-    def test_failed_logout(self):
-        """on post response with error status, KeyCloakError is raised"""
-        pass
-
-    def test_using(self):
-        """login and logout are called"""
-        pass
-
-    def test_failed_using(self):
-        """if an exception during using then it is propogated correctly"""
-        pass
-
-    def test_failed_using_login(self):
-        """if login fails, logout is not called"""
-        pass
-
-    def test_failed_using_logout(self):
-        """if logout fails, no exception is generated"""
-        pass
 
     # DP TODO: the test_(get/post/put/delete) test logic can be merged into a
     #          single unified method implementing most of the logic
@@ -162,7 +283,7 @@ class TestKeyCloakClient(unittest.TestCase):
 
         url = BASE + '/admin/realms/' + REALM + '/' + suffix
         params = {'param': 'one'}
-        headers = {'header': 'one', 'Authorization': 'Bearer xxxxxx'}
+        headers = {'header': 'one', 'Authorization': 'Bearer access_token'}
         call = mock.call(url, headers = headers, params = params, verify = True)
         self.assertEqual(mGet.mock_calls, [call])
 
@@ -190,7 +311,7 @@ class TestKeyCloakClient(unittest.TestCase):
         url = BASE + '/admin/realms/' + REALM + '/' + suffix
         data = {'data': 'one'}
         headers = {'header': 'one',
-                   'Authorization': 'Bearer xxxxxx',
+                   'Authorization': 'Bearer access_token',
                    'Content-Type': 'application/json'}
         call = mock.call(url, headers = headers, data = data, verify = True)
         self.assertEqual(mPost.mock_calls, [call])
@@ -219,7 +340,7 @@ class TestKeyCloakClient(unittest.TestCase):
         url = BASE + '/admin/realms/' + REALM + '/' + suffix
         data = {'data': 'one'}
         headers = {'header': 'one',
-                   'Authorization': 'Bearer xxxxxx',
+                   'Authorization': 'Bearer access_token',
                    'Content-Type': 'application/json'}
         call = mock.call(url, headers = headers, json = data, verify = True)
         self.assertEqual(mPut.mock_calls, [call])
@@ -248,7 +369,7 @@ class TestKeyCloakClient(unittest.TestCase):
         url = BASE + '/admin/realms/' + REALM + '/' + suffix
         data = {'data': 'one'}
         headers = {'header': 'one',
-                   'Authorization': 'Bearer xxxxxx',
+                   'Authorization': 'Bearer access_token',
                    'Content-Type': 'application/json'}
         call = mock.call(url, headers = headers, data = data, verify = True)
         self.assertEqual(mDelete.mock_calls, [call])
@@ -261,3 +382,14 @@ class TestKeyCloakClient(unittest.TestCase):
         with self.assertRaises(KeyCloakError):
             with self.client as kc:
                 response = kc._delete('test')
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self.client = KeyCloakClient(REALM, BASE)
+
+        # Have to use MethodType to bind the mock method to the specific instance
+        # of the KeyCloakClient instead of binding it to all instances of the
+        # class
+        # See http://stackoverflow.com/questions/972/adding-a-method-to-an-existing-object-instance
+        self.client.login = MethodType(mock_login, self.client)
+        self.client.logout = MethodType(mock_logout, self.client)
