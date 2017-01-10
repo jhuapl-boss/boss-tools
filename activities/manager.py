9bcb4156055e9353d972d0881803d98cd34f7b96
Derek Pryor <Derek.Pryor@jhuapl.edu>
2017-01-10 07:22:54 -0800

    Initial framework for StepFunction activity server

---
+++ activities/manager.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# Copyright 2016 The Johns Hopkins University Applied Physics Laboratory
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import bossutils
+from heaviside.activities import ActivityManager, ActivityProcess, TaskProcess
+
+bossutils.utils.set_excepthook()
+
+def pass(input_):
+    return input_
+
+class BossActivityManager(ActivityManager):
+    def __init__(self):
+        super().__init__(self)
+        config = bossutils.configuration.BossConfig()
+
+        self.domain = config['system']['fqdn'].split('.', 1)
+
+    def build(self):
+        def dispatch(target):
+            def wrapped(*args, **kwargs):
+                return TaskProcess(*args, target=target, **kwargs)
+            return wrapped
+
+        return [
+            #lambda: ActivityProcess('Name.'+self.domain, dispatch(function))
+            lambda: ActivityProcess('Pass.'+self.domain, dispatch(input_))
+        ]
+
+if __name__ == '__main__':
+    mgr = BossActivityManager()
+    mgr.run()
+
