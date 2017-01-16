d7497051ef325374fe9732c96f61b172568798a5
Tim Gion <movestill@gmail.com>
2017-01-16 11:11:03 -0500

    Test updates.

--- jenkins.sh
+++ jenkins.sh
@@ -2,4 +2,7 @@
 
 # Set this environment variable so non-unit tests can be skipped.
 export NOSE_UNIT_TESTS_RUNNING=1
+
+# Set path to dependencies.
+set PYTHONPATH=$WORKSPACE/../../spdb/workspace:$WORKSPACE/../../ndingest/workspace
 nose2
