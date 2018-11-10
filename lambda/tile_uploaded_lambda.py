b36382e11f34df88428afb5cf171edf955a4f0df
Tim Gion <movestill@gmail.com>
2018-11-09 23:18:18 -0500

    Check if chunk already full and exit out if so.
    Under heavy load, may put the same chunk on the ingest queue multiple
    times, so check if chunk is already full before proceeding.

--- lambda/tile_uploaded_lambda.py
+++ lambda/tile_uploaded_lambda.py
@@ -65,6 +65,9 @@ def handler(event, context):
     tile_index_db = BossTileIndexDB(proj_info.project_name)
     chunk = tile_index_db.getCuboid(metadata["chunk_key"], int(metadata["ingest_job"]))
     if chunk:
+        if tile_index_db.cuboidReady(metadata["chunk_key"], chunk["tile_uploaded_map"]):
+            print("Chunk {} already has all its tiles, aborting.".format(metadata["chunk_key"]))
+            return
         print("Updating tile index for chunk_key: {}".format(metadata["chunk_key"]))
         chunk_ready = tile_index_db.markTileAsUploaded(metadata["chunk_key"], tile_key, int(metadata["ingest_job"]))
     else:
