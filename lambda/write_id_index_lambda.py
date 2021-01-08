8f8ff0e60325dde8b26954a8e548e944107da655
Tim Gion <movestill@gmail.com>
2021-01-08 17:51:24 -0500

    Remove ids that were successfully updated.
    Don't redo completed work in case the lambda has to be retried.  The
    controlling step function will pass the lambda's return value to the
    next invocation of the lambda when retrying.

--- lambda/write_id_index_lambda.py
+++ lambda/write_id_index_lambda.py
@@ -83,11 +83,15 @@ def handler(event, context):
         s3_index_table, id_index_table, id_count_table, cuboid_bucket, 
         get_region())
 
+    # Track which ids successfully updated.
+    done_ids = set()
+
     try:
         for obj_id in event['id_group']:
             obj_ind.write_id_index(
                 id_index_new_chunk_threshold, 
                 event['cuboid_object_key'], obj_id, event['version'])
+            done_ids.add(obj_id)
         write_id_index_status['done'] = True
     except botocore.exceptions.ClientError as ex:
         # Probably had a throttle or a ConditionCheckFailed.
@@ -99,6 +103,8 @@ def handler(event, context):
             raise DynamoClientError(msg) from ex
         event['result'] = str(ex)
         prep_for_retry(write_id_index_status)
+
+    event['id_group'] = [i for i in event['id_group'] if i not in done_ids]
 
     return event
 
