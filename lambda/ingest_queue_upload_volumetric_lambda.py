e82277195720385e02ec30bedd8bab1c57b3482b
Sandy Hider <sandy.hider@jhuapl.edu>
2022-01-11 13:02:46 -0500

    added missing arg items_to_skip

--- lambda/ingest_queue_upload_volumetric_lambda.py
+++ lambda/ingest_queue_upload_volumetric_lambda.py
@@ -50,6 +50,7 @@ def handler(args, context):
 
             'z_chunk_size': 64, or other possible number
             'MAX_NUM_ITEMS_PER_LAMBDA': 20000
+            'items_to_skip': 0    number of chunks to skip
         }
 
     Returns:
@@ -156,7 +157,7 @@ def create_messages(args):
     """Create all of the tile messages to be enqueued.  Currently not support t extent.
 
     Args:
-        args (dict): Same arguments as populate_upload_queue()
+        args (dict): Same arguments as handler()
 
     Returns:
         list: List of strings containing Json data
